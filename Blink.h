2524cd6f511865ce3328925a4a1919aff41e9994
younghoopark <37133760+younghoopark@users.noreply.github.com>
2018-03-09 02:19:07 +0900

    Add files via upload

---
+++ Blink.h
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+
+class Blink
+{
+  public:
+    Blink(int pin);
+    void on();
+    void off();
+  private:
+    int _pin;
+};
+
---
+++ blink.ino
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+#include "Blink.h"
+
+Blink blink(D4);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("\nBlink Version 1.0 YoungHoo Park");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  blink.on();
+  blink.off();
+}
