6b10213f1c95a2d103b164a63b300116fb9bb11e
younghoopark <37133760+younghoopark@users.noreply.github.com>
2018-03-09 02:28:44 +0900

    Add files via upload

---
+++ Blink.cpp
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+#include "Blink.h"
+
+Blink::Blink(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+
+void Blink::on(int a)
+{
+  digitalWrite(_pin, HIGH);
+  delay(1000);
+  digitalWrite(_pin, LOW);
+  delay(1000);
+}
+
+void Blink::off(int b)
+{
+  digitalWrite(_pin, HIGH);
+  delay(1000);
+  digitalWrite(_pin, LOW);
+  delay(1000);
+}
+
---
+++ Blink.h
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+
+class Blink
+{
+  public:
+    Blink(int pin);
+    void on(int a);
+    void off(int b);
+  private:
+    int _pin;
+};
+
